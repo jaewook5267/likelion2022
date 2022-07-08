b608e58da9daf29739cee8bba993329069a85f55
kangdukyoung <kang48450@gmail.com>
2022-07-09 01:57:08 +0900

    ready

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+import shop.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', shop.views.home, name='home'),
+    path('shop/', include('shop.urls')),
+]
