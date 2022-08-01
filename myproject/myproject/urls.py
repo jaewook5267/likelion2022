5d642a7168e0cbd8832c5408aba15e1eefd16067
ghkdwp018 <ghkdwp018@naver.com>
2022-08-01 14:30:41 +0900

    baco

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,7 +6,8 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', shop.views.home, name='home'),
     path('shop/', include('shop.urls')),
-    path('accounts/', include('accounts.urls')), # 유저 관리
+    path('accounts/', include('accounts.urls')), # 계정 관리
     path('accounts/', include('allauth.urls')), # 소셜 로그인
 ]
